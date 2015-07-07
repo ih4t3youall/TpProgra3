977dbca486d4cff5c8e743c4533330811673216a
ih4t3youall <martinlequerica@outlook.com>
2015-07-07 15:40:58 -0300

    se actualiza la clase comparador para funcionar con la priority queue que todavia no esta implementadoa aviso , esta version no funciona

--- src/comparador/Comparador.java
+++ src/comparador/Comparador.java
@@ -6,14 +6,18 @@ import mapa.Nodo;
 
 public class Comparador implements Comparator<Nodo> {
 
-
 	@Override
 	public int compare(Nodo o1, Nodo o2) {
-
+//		return o1.getDistancia() -o2.getDistancia();
 		
-
-			return o1.getDistancia() -o2.getDistancia();
-
+		if (o1.getDistancia() < o2.getDistancia()) {
+			return 1;
+		}
+		
+		if (o1.getDistancia() > o2.getDistancia()) {
+			return -1;
+		}
+		return 0;
 		
 	}
 
